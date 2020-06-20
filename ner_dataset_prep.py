8ba9ce8918d56d55f303a117c718efd0296cc79c
gopuvenkat <gopuvenkat@yahoo.com>
2020-06-20 11:48:34 +0200

    NER dataset to be BIO tagged

---
+++ ner_dataset_prep.py
@@ -0,0 +1,28 @@
+import os
+from tqdm import tqdm
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+path = '/raid/seid/par4sem/amharic/COLING/ner/nmsu-say'
+dataset_list = list()
+
+for file in tqdm(os.listdir(path)):
+    if not file.endswith('xml'):
+        continue
+    with open(path+'/'+file, 'r') as f:
+        for line in f:
+            if line.strip().endswith('<br />'):
+                soup = BeautifulSoup(line.strip(), 'lxml')
+                text = soup.text.strip()
+                annotations = list()
+                for tag in soup.find_all('a'):
+                    begin = int(soup.text.index(tag.text))
+                    end = len(tag.text) + begin
+                    annotations.append((text[begin:end], tag['title']))
+                    tag.unwrap()
+                if(text and annotations):
+                    dataset_list.append([text, annotations])
+
+df = pd.DataFrame(dataset_list, columns=['text', 'annotation'])
+df.to_csv('ner_data.csv', index=False)
