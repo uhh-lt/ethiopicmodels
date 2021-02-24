73d00bdd19c727fe7e0dd11205c945caf8e87cde
seyyaw <seidymam@gmail.com>
2021-02-24 23:54:24 +0100

    Update yearlygrowth.php

--- amharicfactcheck/yearlygrowth.php
+++ amharicfactcheck/yearlygrowth.php
@@ -208,6 +208,6 @@
 
 		<script type="text/javascript" src="https://canvasjs.com/assets/script/jquery-1.11.1.min.js"></script>
 
-	<script type="text/javascript" src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
+	<script src="lib/jquery.canvasjs.min.js"></script>
 
 </body></html>
