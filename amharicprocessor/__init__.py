1f1c9d17129389d6f77946a9b5dbc99fa9b26e2d
Seid Muhie Yimam <seidymam@gmail.com>
2021-10-31 01:24:32 +0200

    preparing for packaging

---
+++ amharicprocessor/__init__.py
@@ -0,0 +1,3 @@
+from amharicprocessor.amharicSegmenter import AmharicSegmenter
+from amharicprocessor.amharicNormalizer import AmharicNormalizer
+from amharicprocessor.amharicRomanizer import AmharicRomanizer
